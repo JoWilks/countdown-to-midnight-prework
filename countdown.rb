23af4bc5aff745dffd8473ee3bb920d4bc2411c8
joannewilkins88@gmail.com <joannewilkins88@gmail.com>
2018-07-02 11:58:33 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -4,10 +4,10 @@ def countdown(number)
   
   while number>0 
     countdown_with_sleep()
-    puts "#{number}"
+    puts "#{number} SECOND(S)!"
     number -= 1
   end
-  puts "HAPPY NEW YEAR!"
+  "HAPPY NEW YEAR!"
 end
  
 def countdown_with_sleep(number = 1)
